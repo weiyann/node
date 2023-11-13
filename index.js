d1a3b1aa468efb19b2e1dd974b10ed84a3c666e4
weiyann <jona11105@gmail.com>
2023-11-14 00:16:17 +0800

    a.html

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
   res.send('<h2>abc</h2>');
 });
 
+// app.get("/a.html", (req, res) => {
+//   res.send(`假的 a.html`);
+// });
+
+// 設定靜態內容的資料夾
+app.use(express.static("public"));
+
 // 404 // 要放在別的路由後面 // .use是所有的方法
 app.use((req, res) => {
   res.status(404).send(`<h1>你迷路了</h1>`)
@@ -15,6 +22,6 @@ app.use((req, res) => {
 
 const port = process.env.WEB_PORT || 3001; // 如果沒設定就使用3001
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`express server ${port}`)
 })
