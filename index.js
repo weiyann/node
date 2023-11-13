f55da504d7a996d18445b4a2dbfe0cc5db2c370a
weiyann <jona11105@gmail.com>
2023-11-13 15:53:05 +0800

    404 page

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import express from "express";
 
 const app = express();
 
-// 定義路由
+// 定義路由,允許get方法拜訪
 app.get('/', (req, res) => {
   res.send('<h2>abc</h2>');
 });
 
-const port = process.env.WEB_PORT || 3001; // 如果沒設定就是3001
+// 404 // 要放在別的路由後面 // .use是所有的方法
+app.use((req, res) => {
+  res.status(404).send(`<h1>你迷路了</h1>`)
+})
+
+const port = process.env.WEB_PORT || 3001; // 如果沒設定就使用3001
 
 app.listen(3000, () => {
   console.log(`express server ${port}`)
