f711b158e03851161c42f5dedabd0dd2c4486fbd
weiyann <jona11105@gmail.com>
2023-11-16 11:06:24 +0800

    算總頁數

--- routes/address-book.js
+++ routes/address-book.js
@@ -3,17 +3,21 @@ import db from './../utils/connect-mysql.js';
 
 const router = express.Router();
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
 
   const perPage = 20; // 每頁幾筆
-  let page;
+  let page = +req.query.page || 1;
   let totalRows;
   let totalPages;
   let rows = [];
 
   const t_sql = "select count(1) totalRows from address_book";
-  [[{totalRows}]] = await db.query(t_sql);
-  res.json(totalRows);
+  [[{ totalRows }]] = await db.query(t_sql);
+  totalPages = Math.ceil(totalRows / perPage);
+  res.json({
+    page,
+    totalRows,
+    totalPages});
 
   // const sql = "SELECT * FROM address_book ORDER BY sid DESC LIMIT 5";
   // const [rows] = await db.query(sql);
