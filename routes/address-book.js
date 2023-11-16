0cf16e25e91d11931475cf2082b0c085a9c2b792
weiyann <jona11105@gmail.com>
2023-11-16 10:55:37 +0800

    求總筆數

--- routes/address-book.js
+++ routes/address-book.js
@@ -4,9 +4,20 @@ import db from './../utils/connect-mysql.js';
 const router = express.Router();
 
 router.get('/', async (req,res)=>{
-  const sql = "SELECT * FROM address_book ORDER BY sid DESC LIMIT 5";
-  const [rows] = await db.query(sql);
-  res.json(rows)
+
+  const perPage = 20; // 每頁幾筆
+  let page;
+  let totalRows;
+  let totalPages;
+  let rows = [];
+
+  const t_sql = "select count(1) totalRows from address_book";
+  [[{totalRows}]] = await db.query(t_sql);
+  res.json(totalRows);
+
+  // const sql = "SELECT * FROM address_book ORDER BY sid DESC LIMIT 5";
+  // const [rows] = await db.query(sql);
+  // res.json(rows)
 })
 
 export default router;
